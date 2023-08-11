f7d880e79009c4558858e7b7c451d1d223234bf7
Piotr Ochodek <ohis@op.pl>
2023-08-11 09:01:08 +0200

    Js-koszykSklepowy modul 3.2 query selector

---
+++ JS-od-podstaw/Koszyk sklepowy/src/js/app.js
@@ -0,0 +1,35 @@
+const product1 = { price: 10, title: 'JS od podstaw' };
+const product2 = { price: '20', title: 'PHP od podstaw' };
+const discount = 10;
+
+//dodaj produkty do tabeli
+const itemsContainer = document.querySelector('#items');
+let counter = 1;
+
+function addItem(item) {
+  itemsContainer.innerHTML += 
+  `<tr>
+    <td>${counter++}</td>
+    <td>${item.title}</td>
+    <td>1</td>
+    <td>${item.price}</td>
+  </tr>`;
+}
+
+addItem(product1);
+addItem(product2);
+
+if(isNaN(+product2.price)) {
+  console.log('Podano niepoprawny typ danych');
+}
+
+const total = Number(product1.price) + Number(product2.price);
+const totalWithDiscount = total - 10;
+
+//cena całkowita
+document.querySelector('#total-price').innerHTML = total
+
+console.log(`
+  Cena przed zniżką: ${total}
+  Cena po zniżce: ${totalWithDiscount}
+`);
