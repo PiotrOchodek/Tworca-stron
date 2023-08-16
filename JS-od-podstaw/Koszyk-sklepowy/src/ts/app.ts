7e8c00f1aa1972897f6566083e37e35f094d4461
Piotr Ochodek <ohis@op.pl>
2023-08-16 10:20:19 +0200

    Js-koszykSklepowy modul 3.6 adEventListener. Dodatkowo podpięcie TS pod projekt

---
+++ JS-od-podstaw/Koszyk-sklepowy/src/ts/app.ts
@@ -0,0 +1,43 @@
+const product1 = { price: 10, title: 'JS od podstaw' };
+const product2 = { price: '20', title: 'PHP od podstaw' };
+const discount = 10;
+let discountEnabled = false;
+
+//dodaj produkty do tabeli
+const itemsContainer = document.querySelector('#items');
+let counter = 1;
+
+function addItem(item) {
+  itemsContainer.innerHTML += 
+  `<tr>
+    <td>${counter++}</td>
+    <td>${item.title}</td>
+    <td>1</td>
+    <td>${item.price}</td>
+  </tr>`;
+}
+
+addItem(product1);
+addItem(product2);
+
+//dodaj zniżkę
+function addDiscount() {
+  discountEnabled = window.event.target.checked;
+  if(discount > 0) {
+    document.querySelector('#discount-amount').innerHTML = -discount;
+    document.querySelector('#discount').classList.toggle('hidden');
+  }
+  calculatePrice()
+}
+
+//cena całkowita
+function calculatePrice() {
+  let total = Number(product1.price) + Number(product2.price);
+  if(discountEnabled) {
+    total -= discount;
+  }
+
+  document.querySelector('#total-price').innerHTML = total;
+}
+
+calculatePrice()
